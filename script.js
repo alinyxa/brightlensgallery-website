d6e6580d3d1162d9285acf5b85b0641788618cde
alinyxa <tokaralina87@gmail.com>
2026-04-25 22:27:20 +0200

    Initial website upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+}
+
+const form = document.getElementById("contactForm");
+
+if (form) {
+    form.addEventListener("submit", function (e) {
+
+        const name = form.elements["name"].value.trim();
+        const phone = form.elements["phone"].value.trim();
+
+        if (name.length < 2) {
+            alert("Введіть коректне ім’я");
+            e.preventDefault();
+            return;
+        }
+
+        if (phone.length < 10) {
+            alert("Введіть коректний номер телефону");
+            e.preventDefault();
+            return;
+        }
+
+    });
+}
+function showSuccessMessage() {
+    const msg = document.getElementById("successMessage");
+
+    msg.classList.add("show");
+
+    setTimeout(() => {
+        msg.classList.remove("show");
+    }, 3000);
+}
